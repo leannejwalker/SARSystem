36e0737a084b77c3592a71130162803e35228617
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 14:19:55 +0100

    Update all pages

---
+++ book-a-repair.php
@@ -0,0 +1,31 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<script language="JavaScript" type="text/javascript" src="/js/jquery-3.6.0.js"></script>
+  <head>
+      <meta charset="UTF-8">
+      <title>Book a Repair - Share and Repair</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <style>
+
+  </style>
+
+  <body>
+  <?php include "header.php"?>
+
+    
+    <script>
+
+    </script>
+  </body>
+</html>
