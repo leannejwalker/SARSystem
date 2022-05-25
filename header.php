42d6304362d3f3da28945d565359116fa278721c
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 11:49:34 +0100

    More css changes

--- header.php
+++ header.php
@@ -4,148 +4,120 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body {margin:0;font-family:Arial}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  -ms-overflow-style: none;
+  scrollbar-width: none; 
 
-.topnav {
-  overflow: hidden;
-  background-color: #333;
 }
 
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.active {
-  background-color: #04AA6D;
-  color: white;
-}
-
-.topnav .icon {
+body::-webkit-scrollbar {
   display: none;
 }
 
-.dropdown {
+img{
   float: left;
-  overflow: hidden;
+  position: relative;
+  block-size: 3em;
+  padding: 8px;
+  margin-left: 18px;
+  margin-right:24px;
 }
 
-.dropdown .dropbtn {
-  font-size: 17px;    
+.navbar {
+  overflow: hidden;
+  background-color: rgb(0, 0, 0); 
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 24px 24px;
+  text-decoration: none;
+}
+.navbar a#cart {
+  float: right;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 24px 24px;
+  text-decoration: none;
+}
+
+.subnav {
+  float: left;
+  overflow: hidden;
+  display: block;
+}
+
+.subnav .subnavbtn {
+  font-size: 16px;  
   border: none;
   outline: none;
   color: white;
-  padding: 14px 16px;
+  padding: 24px 24px;
   background-color: inherit;
   font-family: inherit;
   margin: 0;
 }
 
-.dropdown-content {
+.navbar a:hover, .subnav:hover .subnavbtn {
+  background-color: rgb(65, 65, 65);
+}
+
+.subnav-content {
+  min-width: 120px;
   display: none;
   position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  left: 1;
+  background-color: rgb(65, 65, 65);
   z-index: 1;
 }
 
-.dropdown-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
+.subnav-content a {
+  min-width: 120px;
+  text-align: left;
+  float: left 15px;
+  color: white;
   text-decoration: none;
   display: block;
-  text-align: left;
 }
 
-.topnav a:hover, .dropdown:hover .dropbtn {
-  background-color: #555;
-  color: white;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
+.subnav-content a:hover {
+  background-color: #eee;
   color: black;
 }
 
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child), .dropdown .dropbtn {
-    display: none;
-  }
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-  .topnav.responsive .dropdown {float: none;}
-  .topnav.responsive .dropdown-content {position: relative;}
-  .topnav.responsive .dropdown .dropbtn {
-    display: block;
-    width: 100%;
-    text-align: left;
-  }
+.subnav:hover .subnav-content {
+  display: flex;
+  flex-direction: column;
 }
 </style>
 </head>
 <body>
 
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
+<div class="navbar">
+		<img src="img/tmb.png" alt="The Mighty Blob">
+		<a href="index.html">Home</a>
+		<div class="subnav">
+		  <button class="subnavbtn">About <i class="fa fa-caret-down"></i></button>
+			<div class="subnav-content">
+				<a href="company.html">About The Mighty Blob</a>
+				<a href="faq.html">FAQs</a>
+			</div>
+		</div> 
+		<a href="repair.html">Repair</a>
+		<a href="shop.html">Shop</a>
 
-<div style="padding-left:16px">
-  <h2>Responsive Topnav with Dropdown</h2>
-  <p>Resize the browser window to see how it works.</p>
-  <p>Hover over the dropdown button to open the dropdown menu.</p>
-</div>
 
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
+		<a href="contact.html">Contact</a>
 
+
+		<a class="fa" id="cart">&#xf07a;</a>
+		&#xf2bd;
+	</div>
 </body>
 </html>
