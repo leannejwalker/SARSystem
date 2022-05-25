ba0235c9bcfa5e5c814667be6e30f14bcc37b70b
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 14:54:27 +0100

    Remove forename validation

--- register.php
+++ register.php
@@ -43,35 +43,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-
-        // Validate username
-        if(empty(trim($_POST["forename"]))){
-            $forename_err = "Please enter your forname.";
-        } elseif(!preg_match('/^[a-zA-Z_]+$/', trim($_POST["forename"]))){
-            $forename_err = "Forename can only contain letters.";
-        } else{
-            // Prepare a select statement
-            $sql = "SELECT id FROM users WHERE forename = ?";
-            
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "s", $param_forename);
-                
-                // Set parameters
-                $param_forename = trim($_POST["forename"]);
-                
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    /* store result */
-                    mysqli_stmt_store_result($stmt);
-                } else{
-                    $forename = trim($_POST["forename"]);
-                }
-    
-                // Close statement
-                mysqli_stmt_close($stmt);
-            }
-        }
     
     // Validate password
     if(empty(trim($_POST["password"]))){
