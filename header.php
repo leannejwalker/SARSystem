34738bf264b94ed11036652b187c310c806251a3
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 11:32:11 +0100

    Change CSS

--- header.php
+++ header.php
@@ -1,83 +1,81 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-    
-    body {
-    font-family: Arial, Helvetica, sans-serif;
-    }
+/* The navigation menu */
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
 
-    .navbar {
-        overflow: hidden;
-        background-color: #333;
-        font-family: Arial;
-    }
+/* Navigation links */
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
 
-    .navbar a {
-        float: left;
-        font-size: 16px;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
+/* The subnavigation menu */
+.subnav {
+  float: left;
+  overflow: hidden;
+}
 
-    .dropdown {
-        float: left;
-        overflow: hidden;
-    }
+/* Subnav button */
+.subnav .subnavbtn {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
 
-    .dropdown .dropbtn {
-        font-size: 16px;
-        border: none;
-        outline: none;
-        color: white;
-        padding: 14px 16px;
-        background-color: inherit;
-        font-family: inherit; /* Important for vertical align on mobile phones */
-        margin: 0; /* Important for vertical align on mobile phones */
-    }
+/* Add a red background color to navigation links on hover */
+.navbar a:hover, .subnav:hover .subnavbtn {
+  background-color: red;
+}
 
-    /* Add a red background color to navbar links on hover */
-    .navbar a:hover, .dropdown:hover .dropbtn {
-        background-color: red;
-    }
+/* Style the subnav content - positioned absolute */
+.subnav-content {
+  display: none;
+  position: absolute;
+  left: 0;
+  background-color: red;
+  width: 100%;
+  z-index: 1;
+}
 
-    /* Dropdown content (hidden by default) */
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        z-index: 1;
-    }
+/* Style the subnav links */
+.subnav-content a {
+  float: left;
+  color: white;
+  text-decoration: none;
+}
 
-    /* Links inside the dropdown */
-    .dropdown-content a {
-        float: none;
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-        text-align: left;
-    }
+/* Add a grey background color on hover */
+.subnav-content a:hover {
+  background-color: #eee;
+  color: black;
+}
 
-    /* Add a grey background color to dropdown links on hover */
-    .dropdown-content a:hover {
-        background-color: #ddd;
-    }
-
-    /* Show the dropdown menu on hover */
-    .dropdown:hover .dropdown-content {
-        display: block;
-    }
+/* When you move the mouse over the subnav container, open the subnav content */
+.subnav:hover .subnav-content {
+  display: block;
+}
 </style>
+</head>
+<body>
 <div class="navbar">
-		<img src="img/sar.png" alt="Share and Repair">
-		<a href="account.php">Account Details</a>
-		
+	<img src="img/sar.png" alt="Share and Repair">
+	<a href="account.php">Account Details</a>
         <div class="subnav">
 		  <button class="subnavbtn">Borrow<i class="fa fa-caret-down"></i></button>
 			<div class="subnav-content">
@@ -105,12 +103,10 @@
                 <a href="faq.php">FAQ</a>
 				<a href="support.php">Contact Support</a>
 			</div>
-		</div>
-
+        </div>
 		<a href="faq.php">FAQ</a>
-
-
 		<a class="fa" id="cart">&#xf07a;</a>
-		&#xf2bd;
-	</div>
+</div>
+
+</body>
 </html>
