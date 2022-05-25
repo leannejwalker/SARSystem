7a5497d73d33d8696a6b6cec13b14d903996472e
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 12:23:58 +0100

    Add fontawesome kit code

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/059448ac20.js" crossorigin="anonymous"></script>
 <style>
 body {
   font-family: Arial, Helvetica, sans-serif;
