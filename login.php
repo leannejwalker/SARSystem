5e1b3c844f5913593df4b24a85c54578a3e948fb
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-25 14:42:17 +0100

    Change (to check login)

--- login.php
+++ login.php
@@ -118,7 +118,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>    
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <input type="password" name="password" autocomplete="on" class="form-control<?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
