9020ce690812f48339b721822aa376f2efddca26
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-26 09:22:32 +0100

    Change how password reset box sits (will do css)

--- account.php
+++ account.php
@@ -102,26 +102,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 						<td><?=$_SESSION['username']?></td>
 					</tr>
 					<tr>
-          <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="account.php">Cancel</a>
-            </div>
-        </form>
-    </div>    
+                    <h2>Reset Password</h2>
+                    <p>Please fill out this form to reset your password.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+                        <div class="form-group">
+                            <label>New Password</label>
+                            <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                            <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                            <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                            <a class="btn btn-link ml-2" href="account.php">Cancel</a>
+                        </div>
+                    </form>
 					</tr>
 					<tr>
 						<td>Email Address:</td>
