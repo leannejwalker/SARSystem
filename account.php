d71c176ea290dac0e2ed604d981d07eb04258bf7
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-27 21:10:51 +0100

    Add php for verified password reset

--- account.php
+++ account.php
@@ -26,30 +26,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
    
     if(password_verify($password, $hashed_password)){
             // Validate new password
-    if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";     
-    } elseif(strlen(trim($_POST["new_password"])) < 6){
-        $new_password_err = "Password must have atleast 6 characters.";
-    } else{
-        $new_password = trim($_POST["new_password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm the password.";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($new_password_err) && ($new_password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
+        if(empty(trim($_POST["new_password"]))){
+            $new_password_err = "Please enter the new password.";     
+        } elseif(strlen(trim($_POST["new_password"])) < 6){
+            $new_password_err = "Password must have atleast 6 characters.";
+        } else{
+            $new_password = trim($_POST["new_password"]);
         }
-    }
+        
+        // Validate confirm password
+        if(empty(trim($_POST["confirm_password"]))){
+            $confirm_password_err = "Please confirm the password.";
+        } else{
+            $confirm_password = trim($_POST["confirm_password"]);
+            if(empty($new_password_err) && ($new_password != $confirm_password)){
+                $confirm_password_err = "Password did not match.";
+            }
+        }
     } else{
         // Password is not valid, display a generic error message
         $login_err = "Invalid password.";
     }
         
     // Check input errors before updating the database
-    if(empty($new_password_err) && empty($confirm_password_err)){
+    if(empty($password_err) && empty($new_password_err) && empty($confirm_password_err)){
         // Prepare an update statement
         $sql = "UPDATE users SET password = ? WHERE id = ?";
         
