6f955a31c4523576374be8db15cac4b612cada76
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-27 22:24:18 +0100

    Overflow hidden

--- login.php
+++ login.php
@@ -100,6 +100,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         body{
             font: 14px sans-serif; 
             background-image: url('img/background.jpg');
+            overflow: hidden;
         }
         .wrapper{
             border: 0.1em solid #ffffff;
@@ -108,6 +109,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             padding-left: 5em;
             border-radius: 1em;
             background: rgba(255, 255, 255, 0.7);
+            overflow: hidden;
         }
     </style>
 </head>
