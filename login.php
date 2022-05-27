ea2120f8171dda57bdca95dc87549e6a19d6b245
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-27 09:35:02 +0100

    "Remember me" tick box

--- login.php
+++ login.php
@@ -122,6 +122,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
+            <input type="checkbox" id="rememberme">Remember me for 7 days
+            </div>
+            <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
