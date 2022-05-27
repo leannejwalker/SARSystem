86597895157cb79c0ee213b0a738630b8209d913
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-27 21:26:45 +0100

    Colour change

--- account.php
+++ account.php
@@ -95,6 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     h1{
         text-align:center;
         text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+        color: #F36F21;
     }
     h2{
         text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
