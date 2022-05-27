01ac3c901cd8b526e018ab6397adaec562a39060
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-27 20:53:55 +0100

    Ask for old password when changing passwords

--- account.php
+++ account.php
@@ -12,13 +12,20 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$new_password = $confirm_password = "";
-$new_password_err = $confirm_password_err = "";
+$password = $new_password = $confirm_password = "";
+$password = $new_password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Validate new password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your current password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+   
+    if(password_verify($password, $hashed_password)){
+            // Validate new password
     if(empty(trim($_POST["new_password"]))){
         $new_password_err = "Please enter the new password.";     
     } elseif(strlen(trim($_POST["new_password"])) < 6){
@@ -35,6 +42,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(empty($new_password_err) && ($new_password != $confirm_password)){
             $confirm_password_err = "Password did not match.";
         }
+    }
+    } else{
+        // Password is not valid, display a generic error message
+        $login_err = "Invalid password.";
     }
         
     // Check input errors before updating the database
@@ -101,18 +112,45 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <?php include "header.php"?>
     <div class="main" id="accountdetails">
         <div> 
-          <h1> Your Account </h1>
-            <h3> Update your Personal Details</h3>
+            <h1> Your Account </h1>
+            <h2> Change Password </h2>
+            <h2>Reset Password</h2>
+            <p>Please fill out this form to reset your password.</p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Old Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div> 
+                <div class="form-group">
+                    <label>New Password</label>
+                    <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                    <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <a class="btn btn-link ml-2" href="account.php">Cancel</a>
+                </div>
+            </form>
+        </div>
+          
+          
+          <!--<h3> Update your Personal Details</h3>
             <div>
                 <div class="form-group">
                     <label>Username</label>
-                    <input type="text" name="username" class="form-control" value="<?=$_SESSION['username']?>" readonly>
+                    <input type="text" name="username" class="form-control" value="<//?=$_SESSION['username']?>" readonly>
                 </div>
                 <div class="form-group">
                     <label>Email</label>
-                    <input type="text" name="email" class="form-control" value="<?=$_SESSION['email']?>" readonly>
+                    <input type="text" name="email" class="form-control" value="<//?=$_SESSION['email']?>" readonly>
                 </div>
-		    </div>
+		    </div>-->
             <h3> Privacy and Security</h3>
         </div>
     </div>
