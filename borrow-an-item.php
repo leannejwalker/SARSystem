ed0d4b7068c6cd99fcc9be6e8dd3b651ebbe1aef
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-28 19:05:38 +0100

    Product card for borrowing items

--- borrow-an-item.php
+++ borrow-an-item.php
@@ -28,16 +28,53 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     border-radius: 1em;
     background: rgba(255, 255, 255, 0.7);
   }
+  .card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
   </style>
 
   <body>
   <?php include "header.php"?>
+  <h2 style="text-align:center">Product Card</h2>
 
-    <script>
+  <div class="card">
+    <img src="/w3images/jeans3.jpg" alt="Denim Jeans" style="width:100%">
+    <h1>Tailored Jeans</h1>
+    <p class="price">$19.99</p>
+    <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+    <p><button>Add to Cart</button></p>
+  </div>
+
+  <script>
 
 
 
 
-    </script>
+  </script>
   </body>
 </html>
