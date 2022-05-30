1cc470d8c878725a6ef837054609ac96c35a6be7
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-30 22:02:46 +0100

    Add sone dumb code to php

--- my-repair-sessions.php
+++ my-repair-sessions.php
@@ -15,8 +15,12 @@ $sql = ("SELECT users.id, repair.userid FROM users INNER JOIN repairs ON users.i
 $sql .= ("SELECT * FROM repairs WHERE $userid='" . ($_SESSION['username']) . "'");
 
 $result = mysqli_multi_query($link, $sql);
+$stmt = $link->prepare($sql); 
+$stmt->execute();
+$result = $stmt->get_result();
+$singleRow = $result->fetch_assoc();
 
-$singleRow = mysqli_fetch_assoc($result);
+//$singleRow = mysqli_fetch_assoc($result);
 
 ?>
 <!DOCTYPE html>
