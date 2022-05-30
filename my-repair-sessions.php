60e60b2b303dec28fb83f29063b0d44316de9a5c
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-30 22:34:39 +0100

    stmt

--- my-repair-sessions.php
+++ my-repair-sessions.php
@@ -15,10 +15,8 @@ $sql = ("SELECT users.id, repair.userid FROM users INNER JOIN repairs ON users.i
 $sql .= ("SELECT * FROM repairs WHERE $userid='" . ($_SESSION['username']) . "'");
 
 $result = mysqli_multi_query($link, $sql);
-$singleRow = $result->get_result();
-
-//fetch_assoc()
-//$singleRow = mysqli_fetch_assoc($result);
+$stmt = $result->get_result();
+$singleRow = mysqli_fetch_assoc($stmt);
 
 ?>
 <!DOCTYPE html>
