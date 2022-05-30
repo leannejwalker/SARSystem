8bde3a0b070e0cbe5a6c0be1b5db5dd7c5a48d7a
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-30 20:51:39 +0100

    $sql1 to make sure the result is correct

--- my-repair-sessions.php
+++ my-repair-sessions.php
@@ -12,8 +12,8 @@ require_once "config.php";
 
 $sql = ("SELECT users.id, repair.userid FROM users INNER JOIN repairs ON users.id=repair.userid");
 
-$sql = ("SELECT * FROM repairs WHERE $userid='" . ($_SESSION['username']) . "'");
-$result=mysqli_query($link,$sql);
+$sql1 = ("SELECT * FROM repairs WHERE $userid='" . ($_SESSION['username']) . "'");
+$result=mysqli_query($link,$sql1);
 $singleRow = mysqli_fetch_assoc($result);
 
 /*$queries = [
