e9e60ad0caacc03bcfc34337348758132f18d66c
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-05-31 19:44:57 +0100

    Add link to ico in header

--- header.php
+++ header.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://kit.fontawesome.com/059448ac20.js" crossorigin="anonymous"></script>
+<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <style>
 body {
   font-family: Arial, Helvetica, sans-serif;
