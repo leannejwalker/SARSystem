33464406c6c3b0a74025c931a81551a6677661d2
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-01 12:30:03 +0100

    Update values

--- book-a-repair.php
+++ book-a-repair.php
@@ -60,45 +60,47 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <form action="new_repair.php" method="post">
     <a style="color:red;">*</a>Item Category: <select id="category" name="category" required>
         <option>===SELECT AN OPTION===</option>
-        <option value="aac">Art, Antiques and Collectables</option>
-        <option value="faa">Fashion and Accessories</option>
-        <option value="elec">Electronics</option>
-        <option value="homeapp">Home Appliances</option>
-        <option value="tag">Toys and Games</option>
-        <option value="oag">Outdoor and Garden</option>
-        <option value="other">Other</option>
+        <option value="Art, Antiques and Collectables">Art, Antiques and Collectables</option>
+        <option value="Fashion and Accessories">Fashion and Accessories</option>
+        <option value="Electronics">Electronics</option>
+        <option value="Home Appliances">Home Appliances</option>
+        <option value="Toys and Games">Toys and Games</option>
+        <option value="Outdoor and Garden">Outdoor and Garden</option>
+        <option value="Other">Other</option>
       </select><br/>
       <a style="color:red;">*</a>Item Name: <input type="text" name = "itemname" required/><br/>
       <a style="color:red;">*</a>Make: <input type="text" name = "make" required/><br/>
       <a style="color:red;">*</a>Model: <input type="text" name = "model" required/><br/>
       <a style="color:red;">*</a>Age: <select id="age" name="age" required>
         <option>===SELECT AN OPTION===</option>
-        <option value="new">Under 1 year</option>
-        <option value="young">Between 1-2 years</option>
-        <option value="used">Between 3-5 years</option>
-        <option value="old">Between 6-10 years</option>
-        <option value="older">Between 11-20 years</option>
-        <option value="vold">Over 21 years</option>
+        <option value="Under 1 year">Under 1 year</option>
+        <option value="Between 1-2 years">Between 1-2 years</option>
+        <option value="Between 3-5 years">Between 3-5 years</option>
+        <option value="Between 6-10 years">Between 6-10 years</option>
+        <option value="Between 11-20 years">Between 11-20 years</option>
+        <option value="Over 21 years">Over 21 years</option>
+        <option value="I'd rather not say">I'd rather not say</option>
       </select><br/>
       <a style="color:red;">*</a>Cost of Item: <select id="cost" name="cost" required>
         <option>===SELECT AN OPTION===</option>
-        <option value="free">Under £5</option>
-        <option value="cheap">Between £5-£10</option>
-        <option value="mid">Between £10-£50</option>
-        <option value="expensive">Between £50-£100</option>
-        <option value="a lot">Over £100</option>
+        <option value="Under £5">Under £5</option>
+        <option value="Between £5-£10">Between £5-£10</option>
+        <option value="Between £10-£50">Between £10-£50</option>
+        <option value="Between £50-£100">Between £50-£100</option>
+        <option value="Over £100">Over £100</option>
+        <option value="I'd rather not say">I'd rather not say</option>
       </select><br/>
       <a style="color:red;">*</a>Details of fault: <input type="text" name = "dof" required/><br/>
       <a style="color:red;">*</a>Is the item out of warranty?: <select id="oow" name="oow" required>
         <option>===SELECT AN OPTION===</option>
-        <option value="yes">Yes</option>
-        <option value="no">No</option>
+        <option value="Yes">Yes</option>
+        <option value="No">No</option>
       </select><br/>
       <a style="color:red;">*</a>Has the item been repaired before?: <select id="prevrepair" name="prevrepair" required>
         <option>===SELECT AN OPTION===</option>
-        <option value="yessar">Yes, with Share and Repair</option>
-        <option value="yes">Yes, elsewhere</option>
-        <option value="no">No</option>
+        <option value="Yes, with Share and Repair">Yes, with Share and Repair</option>
+        <option value="Yes, elsewhere">Yes, elsewhere</option>
+        <option value="No">No</option>
       </select><br/>
       <input type="submit" />
     </form>
