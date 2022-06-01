9c8dcea94987108a5d08076a2268d458755243ca
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-01 11:55:06 +0100

    Add log back

--- my-repair-sessions.php
+++ my-repair-sessions.php
@@ -13,6 +13,7 @@ require_once "config.php";
 $sql = ("SELECT * FROM users LEFT JOIN repairs ON users.id=repairs.userid WHERE $userid='". ($_SESSION['username']) . "'");
 $result = mysqli_query($link, $sql);
 $singleRow = mysqli_fetch_assoc($result);
+print_r($result)
 ?>
 <!DOCTYPE html>
 <html lang="en">
