bd3443d4d55beb065af43fa4f44846bfe27f7ebf
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-02 21:17:18 +0100

    remove log for sql

--- admin_console/account.php
+++ admin_console/account.php
@@ -69,7 +69,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     mysqli_close($link);
 }
 $sql = ("SELECT access_id FROM users WHERE username = ".$username." AND access_id=".$access_id."");
-print_r($sql)
 print_r($access_id)
 ?>
 <!DOCTYPE html>
