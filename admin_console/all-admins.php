971ff1de6757f73bd55354d7ff4cf10e1deefb1b
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-04 00:12:06 +0100

    Modal for customer details

--- admin_console/all-admins.php
+++ admin_console/all-admins.php
@@ -55,6 +55,40 @@ $singleRow = mysqli_fetch_assoc($result);
     tr:nth-child(even) {
       background-color: #f2f2f2;
     }
+    .modal-header {
+      padding: 2px 16px;
+      background-color: #5cb85c;
+      color: white;
+    }
+
+    /* Modal Body */
+    .modal-body {padding: 2px 16px;}
+
+    /* Modal Footer */
+    .modal-footer {
+      padding: 2px 16px;
+      background-color: #5cb85c;
+      color: white;
+    }
+
+    /* Modal Content */
+    .modal-content {
+      position: relative;
+      background-color: #fefefe;
+      margin: auto;
+      padding: 0;
+      border: 1px solid #888;
+      width: 80%;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+      animation-name: animatetop;
+      animation-duration: 0.4s
+    }
+
+    /* Add Animation */
+    @keyframes animatetop {
+      from {top: -300px; opacity: 0}
+      to {top: 0; opacity: 1}
+    }
 </style>
 <body>
     <?php include "header.php"?>
@@ -73,12 +107,52 @@ $singleRow = mysqli_fetch_assoc($result);
           <td><?php echo $report['username']; ?></td>
           <td><?php echo $report['email']; ?></td>
           <td><?php echo $report['phone']; ?></td>
-          <td><a href="">More Details</a></td>
+          <td><a href="./" id="myBtn" >More Details</a></td>
         </tr>
         <?php
           }
         ?>
       </table>
     </div>
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>Modal Header</h2>
+    </div>
+    <div class="modal-body">
+      <p>Some text in the Modal Body</p>
+      <p>Some other text...</p>
+    </div>
+    <div class="modal-footer">
+      <h3>Modal Footer</h3>
+    </div>
+  </div>
+  <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  </script>
 </body>
 </html>
