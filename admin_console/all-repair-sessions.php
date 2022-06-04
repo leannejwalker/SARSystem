fa0e3bd5a89432c7d72be1e42712e9e341427927
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-05 00:22:29 +0100

    "Add a user"

--- admin_console/all-repair-sessions.php
+++ admin_console/all-repair-sessions.php
@@ -23,7 +23,7 @@ $singleRow = mysqli_fetch_assoc($result);
 <script language="JavaScript" type="text/javascript" src="/js/jquery-3.6.0.js"></script>
 <head>
   <meta charset="UTF-8">
-  <title>My Repair Sessions - Share and Repair</title>
+  <title>All Repair Sessions - Share and Repair</title>
   <link rel="icon" type="image/x-icon" href="img/favicon.ico">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
