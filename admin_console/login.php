81b42d7adb9adfd6fd7b0f81e1ac5374b2037e3e
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-04 21:43:23 +0100

    Replace log in with working login

--- admin_console/login.php
+++ admin_console/login.php
@@ -1,13 +1,15 @@
 <?php
 // Initialize the session
 session_start();
-
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: account.php");
+    exit;
+}
+ 
 // Include config file
 require_once "config.php";
- 
-if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: account.php");
-}
  
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -60,7 +62,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
+                            $_SESSION["username"] = $username;                            
+                                                        
+                            // Redirect user to welcome page
+                            header("location: account.php");
 
                         } else{
                             // Password is not valid, display a generic error message
