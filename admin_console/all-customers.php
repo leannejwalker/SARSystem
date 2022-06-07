5be079feb30cd026a6b7aae9eb727b056e69c08f
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-07 21:01:26 +0100

    ...

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -168,8 +168,8 @@ $singleRow = mysqli_fetch_assoc($result);
         filter = input.value.toUpperCase();
         table = document.getElementById("custtable");
         tr = table.getElementsByTagName("tr");
-        for (i = 1, 2, 3; i < tr.length; i++) {
-          td = tr[i].getElementsByTagName("td")[1, 2, 3];
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
           if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
