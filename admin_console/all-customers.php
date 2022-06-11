d8d77720daf634eae53bf5c4242d898c3d501177
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-11 19:46:27 +0100

    jquery

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -26,8 +26,8 @@ $singleRow = mysqli_fetch_assoc($result);
   <title>All Customers - Share and Repair</title>
   <link rel="icon" type="image/x-icon" href="img/favicon.ico">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css"> 
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 
 </head>
 <style>
