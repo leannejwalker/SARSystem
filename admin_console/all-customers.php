6779788ae4224134d2d579b9f788827627e038f3
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-11 20:28:58 +0100

    move datatable

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -33,7 +33,11 @@ $singleRow = mysqli_fetch_assoc($result);
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/b-2.2.3/b-html5-2.2.3/cr-1.5.6/date-1.1.2/fh-3.2.3/kt-2.7.0/r-2.3.0/sc-2.0.6/sb-1.3.3/sp-2.0.1/sl-1.4.0/datatables.min.js"></script>
-
+  <script>
+    $(document).ready( function () {
+      $('#custtable').DataTable();
+    } ); 
+  </script>
 </head>
 <style>
   body {
@@ -215,10 +219,6 @@ $singleRow = mysqli_fetch_assoc($result);
           modal.style.display = "none";
         }
       }
-
-      $(document).ready( function () {
-      $('#custtable').DataTable();
-    } );
     </script>
   </body>
 </html>
