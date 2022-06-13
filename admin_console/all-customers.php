fa71bb5e7f60aed41e35d91d469865b05c54cd7d
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-13 23:25:55 +0100

    ,,,

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -28,10 +28,6 @@ $singleRow = mysqli_fetch_assoc($result);
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css"> 
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/b-2.2.3/b-html5-2.2.3/cr-1.5.6/date-1.1.2/fh-3.2.3/kt-2.7.0/r-2.3.0/sc-2.0.6/sb-1.3.3/sp-2.0.1/sl-1.4.0/datatables.min.css"/>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/b-2.2.3/b-html5-2.2.3/cr-1.5.6/date-1.1.2/fh-3.2.3/kt-2.7.0/r-2.3.0/sc-2.0.6/sb-1.3.3/sp-2.0.1/sl-1.4.0/datatables.min.js"></script>
 </head>
 <style>
   body {
@@ -46,76 +42,7 @@ $singleRow = mysqli_fetch_assoc($result);
     border-radius: 1em;
     background: rgba(255, 255, 255, 0.9);
   }
-  /* table {
-    border-collapse: collapse;
-    border-spacing: 0;
-    width: 100%;
-    border: 3px solid #ddd;
-    margin-left: -4.5em;
-  }
-
-  th, td {
-    text-align: left;
-    padding: 16px;
-  }
-
-  tr:nth-child(even) {
-      background-color: #f2f2f2;
-    }
-   */
-  #myInput {
-    background-image: url('/css/searchicon.png');
-    background-position: 10px 10px;
-    background-repeat: no-repeat;
-    width: 100%;
-    font-size: 16px;
-    padding: 12px 20px 12px 40px;
-    border: 1px solid #ddd;
-    margin-bottom: 12px;
-  }
-     /* The Modal (background) */
-  .modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-    border-radius: 1em;
-  }
-    /* Modal Content/Box */
-  .modal-content {
-    background-color: #fefefe;
-    margin: 15% auto; /* 15% from the top and centered */
-    border: 0.5em solid #F36F21;
-    width: 80%; /* Could be more or less, depending on screen size */
-    border-radius: 1em;
-  }
-
-  /* The Close Button */
-  .close {
-    color: #aaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-    padding: 1em;
-  }
-
-  .close:hover,
-  .close:focus {
-    color: black;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  /* body.modal-content {
-  overflow-y: hidden;
-  } */
-
+  
   img{
     float: left;
     position: relative;
