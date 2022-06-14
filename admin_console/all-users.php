f533a23a6cb781b47560062816c28f703038ade4
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 00:40:46 +0100

    Add plus minus dropdown

--- admin_console/all-users.php
+++ admin_console/all-users.php
@@ -20,7 +20,7 @@
         },
         {
           extend: 'alert',
-          text: 'Export to .csv'
+          text: 'Export to .CSV'
         }
         ]
     });
@@ -32,6 +32,45 @@ $.fn.dataTable.ext.buttons.alert = {
     alert( this.text() );
   }
 };
+    // Add event listener for opening and closing details
+    $('#users tbody').on('click', 'td.dt-control', function () {
+            var tr = $(this).closest('tr');
+            var row = table.row(tr);
+    
+            if (row.child.isShown()) {
+                // This row is already open - close it
+                row.child.hide();
+                tr.removeClass('shown');
+            } else {
+                // Open this row
+                row.child(format(row.data())).show();
+                tr.addClass('shown');
+            }
+        });
+    /* Formatting function for row details - modify as you need */
+    function format(d) {
+        // `d` is the original data object for the row
+        return (
+            '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+            '<tr>' +
+            '<td>Full name:</td>' +
+            '<td>' +
+            d.name +
+            '</td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>Extension number:</td>' +
+            '<td>' +
+            d.extn +
+            '</td>' +
+            '</tr>' +
+            '<tr>' +
+            '<td>Extra info:</td>' +
+            '<td>And any further details here (images etc)...</td>' +
+            '</tr>' +
+            '</table>'
+        );
+    };
 </script>
 <style>
   body {
