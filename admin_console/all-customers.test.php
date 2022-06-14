e378308284b21e92cd1be10764105fdc32f5cabd
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-14 10:12:33 +0100

    .

--- admin_console/all-customers.test.php
+++ admin_console/all-customers.test.php
@@ -31,7 +31,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   .main{
     border: 0.1em solid #ffffff;
     margin: 5em;
-    padding: 3em;
+    padding: 1.5em;
     border-radius: 1em;
     background: rgba(255, 255, 255, 0.9);
   }
