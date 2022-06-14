d4c8e8e7ce0d9982b91520dcf96a6a7a9fddf7fb
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-14 09:19:24 +0100

    ..

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -28,6 +28,7 @@ $singleRow = mysqli_fetch_assoc($result);
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css"> 
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+  <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 </head>
 <style>
   /* body {
