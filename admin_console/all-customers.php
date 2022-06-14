82dc509ac1282792d4cf6cd4e42c49e2462a3814
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-14 09:28:48 +0100

    ..

--- admin_console/all-customers.php
+++ admin_console/all-customers.php
@@ -20,14 +20,14 @@ $singleRow = mysqli_fetch_assoc($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"> 
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <head>
   <meta charset="UTF-8">
   <title>All Customers - Share and Repair</title>
   <link rel="icon" type="image/x-icon" href="img/favicon.ico">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"> 
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-  <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script>
     $(document).ready(function () {
       $('#custtable').DataTable({
@@ -79,45 +79,5 @@ $singleRow = mysqli_fetch_assoc($result);
           </tr>
         </thread>
       </table>
-<!-- 
-    <div id="detailsmodal" class="modal">
-      <div class="modal-content">
-        <div class="navbar">
-          <img href="https://shareandrepair.org.uk" src="img/sar.png" alt="Share and Repair">
-          <span class="close">&times;</span>
-        </div>
-        <p>Some text in the Modal..</p>
-      </div>
-    </div> -->
-
-    <script>
-
-      // // When the user clicks on the button, open the modal
-      // function openModal(user) {
-      //   modal.style.display = "block";
-      // }
-
-      // // Get the modal
-      // var modal = document.getElementById("detailsmodal");
-
-      // // Get the button that opens the modal
-      // var btn = document.getElementById("moredetails");
-
-      // // Get the <span> element that closes the modal
-      // var span = document.getElementsByClassName("close")[0];
-
-
-      // // When the user clicks on <span> (x), close the modal
-      // span.onclick = function() {
-      //   modal.style.display = "none";
-      // }
-
-      // // When the user clicks anywhere outside of the modal, close it
-      // window.onclick = function(event) {
-      //   if (event.target == modal) {
-      //     modal.style.display = "none";
-      //   }
-      // }
-    </script>
   </body>
 </html>
