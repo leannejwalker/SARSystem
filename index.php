1189abaf8b7770c0dc153fa2167995931b5189e6
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 21:09:14 +0100

    Add CSS

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
       <meta charset="UTF-8">
       <title>Your Account - Share and Repair</title>
       <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <script language="JavaScript" type="text/javascript" src="/js/jquery-3.6.0.js"></script>
+      <?php include($_SERVER['DOCUMENT_ROOT']."/sripts/css.php")?>
     </head>
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT']."/src/misc/simple-header.php")?>
