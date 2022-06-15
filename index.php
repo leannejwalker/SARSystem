26dee6828009b2695640cc78c2b54f0e7964239c
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 21:38:11 +0100

    include to require

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<?php include($_SERVER['DOCUMENT_ROOT']."/scripts/config.php")?>
+<?php require($_SERVER['DOCUMENT_ROOT']."/scripts/config.php")?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
       <meta charset="UTF-8">
       <title>Your Account - Share and Repair</title>
       <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-      <?php include($_SERVER['DOCUMENT_ROOT']."/scripts/css.php")?>
+      <?php require($_SERVER['DOCUMENT_ROOT']."/scripts/css.php")?>
     </head>
     <body>
-        <?php include($_SERVER['DOCUMENT_ROOT']."/src/misc/simple-header.php")?>
-        <?php include($_SERVER['DOCUMENT_ROOT']."/src/auth/register.php")?>
-        <?php include($_SERVER['DOCUMENT_ROOT']."/src/auth/login.php")?>
-        <?php include($_SERVER['DOCUMENT_ROOT']."/src/misc/footer.php")?>
+        <?php require($_SERVER['DOCUMENT_ROOT']."/src/misc/simple-header.php")
+        require($_SERVER['DOCUMENT_ROOT']."/src/auth/register.php")
+        require($_SERVER['DOCUMENT_ROOT']."/src/auth/login.php")?>
+        <?php require($_SERVER['DOCUMENT_ROOT']."/src/misc/footer.php")?>
     </body>
+    <footer>
+        <?php require($_SERVER['DOCUMENT_ROOT']."/src/misc/footer.php")?>
+    </footer>
 </html>
