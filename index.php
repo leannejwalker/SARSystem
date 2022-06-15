8c81ac95900e93e1934b7e0d1a193c066c747aa2
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 21:04:50 +0100

    Changes to index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 
-<?php include "/src/misc/simple-header.php"?>
-<?php include "/src/auth/register.php"?>
-<?php include "/src/auth/login.php"?>
-<?php include "/src/misc/footer.php"?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Your Account - Share and Repair</title>
+      <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script language="JavaScript" type="text/javascript" src="/js/jquery-3.6.0.js"></script>
+    </head>
+    <body>
+        <?php include "/src/misc/simple-header.php"?>
+        <?php include "/src/auth/register.php"?>
+        <?php include "/src/auth/login.php"?>
+        <?php include "/src/misc/footer.php"?>
+    </body>
+</html>
