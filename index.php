b25b9def9d6af13fb63d3015d9f390eb0f011449
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 21:11:24 +0100

    add css and config

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-
+<?php include($_SERVER['DOCUMENT_ROOT']."/scripts/config.php")?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
       <meta charset="UTF-8">
       <title>Your Account - Share and Repair</title>
       <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-      <?php include($_SERVER['DOCUMENT_ROOT']."/sripts/css.php")?>
+      <?php include($_SERVER['DOCUMENT_ROOT']."/scripts/css.php")?>
     </head>
     <body>
         <?php include($_SERVER['DOCUMENT_ROOT']."/src/misc/simple-header.php")?>
