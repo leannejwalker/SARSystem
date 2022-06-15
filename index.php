6e61d5508456572e00929686b7d5db6107090022
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-15 22:04:04 +0100

    Set up index php

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 // Initialize the session
 session_start();
+
+require "/scripts/config.php";
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: /src/auth/login.php");
+
+    include "/src/auth/login.php"
+    // header("location: /src/auth/login.php");
     exit;
 }
 
 // Check if the user is logged in, if so then redirect him to the main page
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    header("location: /src/all/account.php");
+    include "/src/all/account.php"
+    // header("location: /src/all/account.php");
     exit;
 }
 ?>
