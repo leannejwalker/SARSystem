c8d496636a1125b3bd099b0edb02b2633d4bb760
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-16 10:55:00 +0100

    Remove document root

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_report
 // Initialize the session
 session_start();
 
-require_once ($_SERVER['DOCUMENT_ROOT']."/scripts/config.php");
+require_once "./scripts/config.php";
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    include ($_SERVER['DOCUMENT_ROOT']."/src/auth/login.php");
+    include "./src/auth/login.php";
     // header("location: /src/auth/login.php");
 }
 
 // Check if the user is logged in, if so then redirect him to the main page
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true){
 
-    include ($_SERVER['DOCUMENT_ROOT']."/pages/all/account.php");
+    include "./pages/all/account.php";
     // header("location: /src/all/account.php");
 }
 ?>
