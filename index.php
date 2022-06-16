5af8a1cd6f8789655cbe3dc44c103bbcb27c8272
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-16 10:00:05 +0100

    Move files

--- index.php
+++ index.php
@@ -7,16 +7,14 @@ require_once ($_SERVER['DOCUMENT_ROOT']."/scripts/config.php");
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    include ($_SERVER['DOCUMENT_ROOT']."/src/auth/login.php")
+    include ($_SERVER['DOCUMENT_ROOT']."/src/auth/login.php");
     // header("location: /src/auth/login.php");
-    exit;
 }
 
 // Check if the user is logged in, if so then redirect him to the main page
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    include ($_SERVER['DOCUMENT_ROOT']."/src/all/account.php")
+    include ($_SERVER['DOCUMENT_ROOT']."/src/all/account.php");
     // header("location: /src/all/account.php");
-    exit;
 }
 ?>
