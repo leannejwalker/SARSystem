02565fd91aa9a8797c8c1c6aabeac229e91b50cc
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-16 11:54:19 +0100

    More changes

--- index.php
+++ index.php
@@ -6,13 +6,12 @@ ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_report
 session_start();
 
 require_once "./scripts/config.php";
-
-include "./scripts/css.php";
-include "./scripts/js.php";
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
+    include "./scripts/css.php";
+    include "./scripts/js.php";
     include "./src/misc/simple-header.php";
     include "./src/auth/login.php";
     include "./src/auth/register.php";
@@ -22,6 +21,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 // Check if the user is logged in, if so then redirect him to the main page
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true){
 
+    include "./scripts/css.php";
+    include "./scripts/js.php";
     include "./pages/customers/account.php";
     // header("location: /src/all/account.php");
 }
