7e687c015d232b75648c26a4dea2c621900c755d
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-16 15:20:33 +0100

    Remove config.php from footer.php

--- src/all/footer.php
+++ src/all/footer.php
@@ -1,20 +1,7 @@
-<?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, otherwise redirect to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://kit.fontawesome.com/059448ac20.js" crossorigin="anonymous"></script>
 <style>
     .footer {
   position: fixed;
