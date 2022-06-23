9b83225d5fdf0597e3721657d108934744c689bf
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-23 17:18:11 +0100

    remove refresh

--- scripts/echo.php
+++ scripts/echo.php
@@ -6,7 +6,6 @@ include "./scripts/css.php";
 include "./scripts/js.php";
 include "./src/misc/simple-header.php";
 include "./src/auth/login.php";
-header("Refresh:0");
 $loginpage = ob_get_contents(); ob_end_clean();
 
 //Logged In
@@ -14,7 +13,6 @@ ob_start();
 include "./src/misc/header.php";
 include "./src/misc/account.php";
 include "./src/misc/footer.php";
-header("Refresh:0");
 $loggedin = ob_get_contents(); ob_end_clean();
 
 ?>
