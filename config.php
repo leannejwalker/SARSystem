449891dad59204b83b8c02c6a18474383ae7dd21
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-24 23:42:13 +0100

    change database details

--- config.php
+++ config.php
@@ -1,16 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '10.9.9.19:3306');
-<<<<<<< HEAD:config.php
-define('DB_USERNAME', 'sarwebsite');
-define('DB_PASSWORD', '3@jDlb89g4d4*As7');
-define('DB_NAME', 'sarwebsite');
-=======
-define('DB_USERNAME', 'devsaruser');
-define('DB_PASSWORD', 'Tv9y5h3&');
-define('DB_NAME', 'devsar');
->>>>>>> 9b83225d5fdf0597e3721657d108934744c689bf:scripts/config.php
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'brob_sar');
+define('DB_PASSWORD', 'Ca-@gsupFPqPIg2J');
+define('DB_NAME', 'brob_sar');
 
 /*$sql = "CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
