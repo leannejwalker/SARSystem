8762b3ad4976167a932438bf1a05925a09a0bf83
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-28 12:47:31 +0100

    move echo

--- src/misc/header.php
+++ src/misc/header.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once "./scripts/config.php";
-require_once "./scripts/echo.php";
 
 $currentid=$_SESSION['id'];
 $sql = ("SELECT * FROM users WHERE id=".trim($currentid)."");
 $result1 = mysqli_query($link, $sql);
 $report1 = mysqli_fetch_assoc($result1);
+
+require_once "./scripts/echo.php";
 
 if (isset($_GET['page'])){
 
