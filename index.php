e6a2cb4c8235e84f6dc0d0003072d0f7d848eef3
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-29 14:11:58 +0100

    echo

--- index.php
+++ index.php
@@ -3,21 +3,7 @@
 // Initialize the session
 session_start();
 require_once "scripts/config.php";
-
-// // Login Page
-// ob_start();
-// include "scripts/css.php";
-// include "scripts/js.php";
-// include "src/misc/simple-header.php";
-// include "src/auth/login.php";
-// $loginpage = ob_get_contents(); ob_end_clean();
-
-//Logged In
-ob_start();
-include "src/misc/header.php";
-include "src/misc/account.php";
-include "src/misc/footer.php";
-$loggedin = ob_get_contents(); ob_end_clean();
+require_once "scripts/echo.php";
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
