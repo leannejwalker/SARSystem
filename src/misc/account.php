4efce35db4ec545f30c23f341e0629184663bd58
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-29 10:01:46 +0100

    Remove refresh

--- src/misc/account.php
+++ src/misc/account.php
@@ -47,7 +47,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             if(mysqli_stmt_execute($stmt)){
                 // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
-                header("Refresh:0");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
