9b1940042290483c2b986522f9e9e7ed3a17afc8
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-29 10:10:06 +0100

    RM

--- scripts/echo.php
+++ scripts/echo.php
@@ -5,7 +5,7 @@ ob_start();
 include "./scripts/css.php";
 include "./scripts/js.php";
 include "./src/misc/simple-header.php";
-include "/src/auth/login.php";
+include "./src/auth/login.php";
 $loginpage = ob_get_contents(); ob_end_clean();
 
 //Logged In
