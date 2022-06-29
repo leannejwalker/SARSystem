1c41657aa822b8ab22a747f26ba9cfb76467db6e
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-29 14:10:31 +0100

    update

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-// // Initialize the session
-// session_start();
-
-// require_once "scripts/config.php";
+// Initialize the session
+session_start();
+require_once "scripts/config.php";
 
 // // Login Page
 // ob_start();
@@ -13,12 +12,12 @@
 // include "src/auth/login.php";
 // $loginpage = ob_get_contents(); ob_end_clean();
 
-// //Logged In
-// ob_start();
-// include "src/misc/header.php";
-// include "src/misc/account.php";
-// include "src/misc/footer.php";
-// $loggedin = ob_get_contents(); ob_end_clean();
+//Logged In
+ob_start();
+include "src/misc/header.php";
+include "src/misc/account.php";
+include "src/misc/footer.php";
+$loggedin = ob_get_contents(); ob_end_clean();
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
