db3d6413416696a7eea85f17c6b1826b84d0bcdd
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-29 23:24:41 +0100

    Updates to website (prettyurls?)

--- index.php
+++ index.php
@@ -2,35 +2,16 @@
 
 // Initialize the session
 session_start();
-require_once "scripts/config.php";
-
-// Login Page
-ob_start();
-include "./scripts/css.php";
-include "./scripts/js.php";
-include "./src/misc/simple-header.php";
-include "./src/auth/login.php";
-$loginpage = ob_get_contents(); ob_end_clean();
-
-//Logged In
-ob_start();
-include "./src/misc/header.php";
-include "./src/misc/account.php";
-include "./src/misc/footer.php";
-$loggedin = ob_get_contents(); ob_end_clean();
 
 // Check if the user is logged in
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true){
 
     // If loggined in, redirect to summary/account page
-    isset($loggedin);
-    echo $loggedin;
+    header('./src/misc/account.php')
 
 }else{
 
-    isset($loginpage);
-    // If not logged in, redirect to login page
-    echo $loginpage;
+    header('./pages/customers/cust-account.php')
 
 };
 ?>
