88193b8367970041ae15d0791093983438c615d7
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-30 19:34:19 +0100

    /

--- pages/customers/cust-account.php
+++ pages/customers/cust-account.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once "/scripts/config.php";
+require_once "./scripts/config.php";
 
 // Initialize the session
 session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: /index.php");
+    header("location: ./index.php");
     exit;
 }
 
