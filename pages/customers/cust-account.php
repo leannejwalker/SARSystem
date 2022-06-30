36833f749a7747ac7b0c2cb3df2a79737b015ecc
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-30 20:14:56 +0100

    /

--- pages/customers/cust-account.php
+++ pages/customers/cust-account.php
@@ -1,12 +1,4 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: ./index.php");
-    exit;
-}
 
 require_once "./scripts/config.php";
 
