c29032df132477b4f3acea4cf1fef1890c0404ae
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-30 19:20:10 +0100

    update

--- index.php
+++ index.php
@@ -2,23 +2,18 @@
 
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
-// Initialize the session
-session_start();
-
-require_once "./scripts/config.php";
-require_once "./scripts/echo.php";
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    echo ($loginpage);
+    header("location: ./src/misc/simple-header.php");
 }
 
 
 // Check if the user is logged in
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true){
 
-    echo ($loggedin);
+    header("location: ./pages/customers/cust-account.php");
 
 }
 ?>
