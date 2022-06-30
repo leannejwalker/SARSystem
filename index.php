6c79be12bc185230dce97fd2d2a6d81b1e22db96
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-30 11:23:02 +0100

    update headers

--- index.php
+++ index.php
@@ -4,35 +4,17 @@
 session_start();
 require_once "scripts/config.php";
 
-// Login Page
-ob_start();
-include "./scripts/css.php";
-include "./scripts/js.php";
-include "./src/misc/simple-header.php";
-include "./src/auth/login.php";
-$loginpage = ob_get_contents(); ob_end_clean();
-
-//Logged In
-ob_start();
-include "./src/misc/header.php";
-include "./pages/customers/cust-account.php";
-include "./src/misc/footer.php";
-$loggedin = ob_get_contents(); ob_end_clean();
 
 // Check if the user is logged in
 if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === true){
 
     // If loggined in, redirect to summary/account page
-    if(isset($loggedin)){
-        echo $loggedin;
-    };
+    header('./pages/customers/cust-account.php');
 
 }else{
 
     // If not logged in, redirect to login page
-    if(isset($loginpage)){
-        echo $loginpage;
-    };
+    header('./src/auth/login.php');
 
 };
 ?>
