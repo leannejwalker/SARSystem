dbef1836ce382d4f54da08d59a8ef5f27ac15f9a
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2022-06-30 19:02:16 +0100

    css js

--- scripts/echo.php
+++ scripts/echo.php
@@ -10,6 +10,8 @@ $loginpage = ob_get_contents(); ob_end_clean();
 
 //Logged In
 ob_start();
+include "./scripts/css.php";
+include "./scripts/js.php";
 include "./src/misc/header.php";
 include "./pages/customers/cust-account.php";
 include "./src/misc/footer.php";
